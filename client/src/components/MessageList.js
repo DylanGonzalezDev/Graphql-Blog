865e76984acfe3d0422ccf9e5496c6f0389e2098
DylanGonzalezDev <dg9927479@gmail.com>
2022-08-09 23:31:48 -0300

    client

---
+++ client/src/components/MessageList.js
@@ -0,0 +1,40 @@
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+export const GET_MESSAGES = gql`
+{
+  messages {
+    _id
+    title
+    content
+    author
+  }
+}
+`;
+
+export default function MessageList() {
+  const { loading, error, data } = useQuery(GET_MESSAGES);
+  console.log(data)
+  if (loading) return <p>Loading Messages...</p>;
+
+  if (error) {
+    console.log(error);
+    return <p>Error</p>;
+  }
+
+  return (
+    <div className="row">
+      <div className="col-md-6 offset-md-3">
+        {data.messages.map(({ _id, title, content, author }) => (
+          <div key={_id} className="card m-2">
+            <div className="card-body">
+              <h4>{title}</h4>
+              <p>{content}</p>
+              <p>{author.username}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
